b580a70d3b09e3e629c4770635cc096442aad129
Oliver Kowalke <oliver.kowalke@gmail.com>
2018-01-05 10:52:43 +0100

    section passing data

---
+++ code/passing_lambda.cpp
@@ -0,0 +1,15 @@
+int i=1;
+std::fiber lambda{
+    [&i](fiber&& caller){
+        std::cout << "inside lambda,i==" << i << std::endl;
+        i+=1;
+        caller=caller.resume();
+        return std::move(caller);
+}};
+lambda=lambda.resume();
+std::cout << "i==" << i << std::endl;
+lambda=lambda.resume();
+
+output:
+    inside lambda,i==1
+    i==2
