8a599bc6a1d4d4437c51ddd59ce4e0f0007ea09b
Oliver Kowalke <oliver.kowalke@gmail.com>
2018-01-14 15:08:16 +0100

    section stl

---
+++ code/generator.cpp
@@ -0,0 +1,24 @@
+int a;
+ctx::fiber g{[&a](ctx::fiber&& m){
+    a=0;
+    int b=1;
+    for(;;){
+        m=m.resume();
+        int next=a+b;
+        a=b;
+        b=next;
+    }
+    return std::move(m);
+}};
+std::vector<int> v(10);
+std::generate(v.begin(), v.end(), [&a,&g]() mutable {
+    g=g.resume();
+    return a;
+});
+std::cout << "v: ";
+for (auto i: v) {
+    std::cout << i << " ";
+}
+std::cout << "\n";
+
+output: v: 0 1 1 2 3 5 8 13 21 34
