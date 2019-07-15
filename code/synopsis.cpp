643c1dd58c150471ec1db50efe1fd5fd6bfe6ab8
Nat Goodspeed <nat@lindenlab.com>
2019-07-15 23:40:24 +0200

    R7 changes: remove unwind_fiber() and all implicit unwinding.
    Instead, require a second fiber_context constructor parameter, an invocable
    which is called by ~fiber_context() for a non-empty instance.

--- code/synopsis.cpp
+++ code/synopsis.cpp
@@ -8,10 +8,6 @@ inline namespace concurrency_v2 {
 
 class fiber_context;
 
-class unwind_exception;
-
-void unwind_fiber(fiber_context&& other);
-
 } // namespace concurrency_v2
 } // namespace experimental
 } // namespace std
