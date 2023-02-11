b4122d71965d80ae575646085e2326203ae9f111
Nat Goodspeed <nat@lindenlab.com>
2023-02-10 22:30:08 -0800

    First pass at responding to LEWG feedback at Issaquah 2023-02.

--- code/synopsis.cpp
+++ code/synopsis.cpp
@@ -1,11 +1,9 @@
 #include <fiber_context>
 
-#define __cpp_lib_fiber_context 202302
+#define __cpp_lib_fiber_context YYYYMM
 
 namespace std {
-inline namespace concurrency_v2 {
 
 class fiber_context;
 
-} // namespace concurrency_v2
 } // namespace std
