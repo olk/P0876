a4e9570a421d4ce8d0df2e6517de84fd4b00c949
Nat Goodspeed <nat@lindenlab.com>
2024-07-03 14:31:38 -0400

    WIP: Add P3346R0 thread_local means fiber-specific

---
+++ code/fiber.tls.cpp
@@ -0,0 +1,38 @@
+namespace std {
+
+class fiber_context {
+public:
+    // [fiber.context.cons], constructors, move and assignment
+    fiber_context() noexcept = default;
+
+    template<class F>
+    explicit fiber_context(F&& entry);
+
+    template<class F, class D>
+    fiber_context(F&& entry, span<byte> stack, D&& deleter);
+
+    ~fiber_context();
+
+    fiber_context(fiber_context&& other) noexcept;
+    fiber_context& operator=(fiber_context&& other) noexcept;
+
+    // [fiber.context.mem], members
+    fiber_context resume() &&;
+    template<class Fn>
+    fiber_context resume_with(Fn&& fn) &&;
+
+    bool can_resume() noexcept;
+
+    explicit operator bool() const noexcept;
+    bool empty() const noexcept;
+
+    void swap(fiber_context& other) noexcept;
+
+    // [fiber.context.special], specialized algorithms
+    friend void swap(fiber_context& lhs, fiber_context& rhs) noexcept;
+
+private:
+    void* state = nullptr;          // exposition only
+};
+
+} // namespace std
---
+++ code/synopsis.tls.cpp
@@ -0,0 +1,9 @@
+namespace std {
+
+    // [fibercontext], class fiber_context
+    class fiber_context;
+
+    template <class T>
+    class thread_specific_ptr;
+
+}
