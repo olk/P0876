77326b9b2830d2e6e39c20747a24191e47e386f5
Nat Goodspeed <nat@lindenlab.com>
2024-10-15 10:25:37 -0400

    Tweaks per ADAM Martin review.

--- code/tsp.cpp
+++ code/tsp.cpp
@@ -10,18 +10,19 @@ public:
 
     ~thread_specific_ptr();
 
-    thread_specific_ptr(const thread_specific_ptr& other) = default;
-    thread_specific_ptr& operator=(const thread_specific_ptr& other) = default;
-    thread_specific_ptr(thread_specific_ptr&& other) = default;
-    thread_specific_ptr& operator=(thread_specific_ptr&& other) = default;
+    thread_specific_ptr(const thread_specific_ptr&) = delete;
+    thread_specific_ptr& operator=(const thread_specific_ptr&) = delete;
+    thread_specific_ptr(thread_specific_ptr&&) = default;
+    thread_specific_ptr& operator=(thread_specific_ptr&&) = default;
 
     // [thread.ptr.mem], members
     T* get() const noexcept;
     T* operator->() const noexcept;
     T& operator*() const noexcept;
 
-    T* release() noexcept;
-    void reset(T* new_value=nullptr);
+    [[nodiscard]] T* release() noexcept;
+    void reset() noexcept;
+    void reset(T* new_value);
 
     explicit operator bool() const noexcept;
 
