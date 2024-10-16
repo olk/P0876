0ba8bb15d5dd25886f9ecca430784ae3dda3a8c3
Nat Goodspeed <nat@lindenlab.com>
2024-10-15 22:19:49 -0400

    Deleter is a template argument; explain move semantics.

--- code/tsp.cpp
+++ code/tsp.cpp
@@ -1,26 +1,24 @@
 namespace std {
 
-template <class T>
+template <class T, class Deleter=default_delete<T>>
 class thread_specific_ptr {
 public:
     // [thread.ptr.cons], constructors, move and assignment
     thread_specific_ptr();
 
-    explicit thread_specific_ptr(void (*cleanup_function)(T*));
+    explicit thread_specific_ptr(const Deleter& deleter);
+    thread_specific_ptr(thread_specific_ptr&& other);
 
     ~thread_specific_ptr();
 
-    thread_specific_ptr(const thread_specific_ptr&) = delete;
-    thread_specific_ptr& operator=(const thread_specific_ptr&) = delete;
-    thread_specific_ptr(thread_specific_ptr&&) = default;
-    thread_specific_ptr& operator=(thread_specific_ptr&&) = default;
+    thread_specific_ptr& operator=(thread_specific_ptr&& other);
 
     // [thread.ptr.mem], members
     T* get() const noexcept;
     T* operator->() const noexcept;
     T& operator*() const noexcept;
 
-    [[nodiscard]] T* release() noexcept;
+    [[nodiscard]] unique_ptr<T, Deleter> release() noexcept;
     void reset() noexcept;
     void reset(T* new_value);
 
@@ -30,6 +28,13 @@ public:
 
     // [thread.ptr.special], specialized algorithms
     friend void swap(thread_specific_ptr& lhs, thread_specific_ptr& rhs) noexcept;
+
+    // disable copy from lvalue
+    thread_specific_ptr(const thread_specific_ptr&) = delete;
+    thread_specific_ptr& operator=(const thread_specific_ptr&) = delete;
+
+private:
+    unique_ptr<T, Deleter> p;       // exposition only
 };
 
 } // namespace std
