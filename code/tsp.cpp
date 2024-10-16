31560ac071a774bc69e652857efd3f144b9816b3
Nat Goodspeed <nat@lindenlab.com>
2024-10-16 09:40:01 -0400

    Disable thread_specific_ptr move and swap as well as copy.
    Make release() return plain T* again.
    
    Mandate is_nothrow_invocable_v<Deleter, T*>.

--- code/tsp.cpp
+++ code/tsp.cpp
@@ -5,36 +5,31 @@ class thread_specific_ptr {
 public:
     // [thread.ptr.cons], constructors, move and assignment
     thread_specific_ptr();
-
     explicit thread_specific_ptr(const Deleter& deleter);
-    thread_specific_ptr(thread_specific_ptr&& other);
+    explicit thread_specific_ptr(Deleter&& deleter);
 
     ~thread_specific_ptr();
-
-    thread_specific_ptr& operator=(thread_specific_ptr&& other);
 
     // [thread.ptr.mem], members
     T* get() const noexcept;
     T* operator->() const noexcept;
     T& operator*() const noexcept;
 
-    [[nodiscard]] unique_ptr<T, Deleter> release() noexcept;
+    [[nodiscard]] T* release() noexcept;
     void reset() noexcept;
     void reset(T* new_value);
 
     explicit operator bool() const noexcept;
 
-    void swap(thread_specific_ptr& other) noexcept;
-
-    // [thread.ptr.special], specialized algorithms
-    friend void swap(thread_specific_ptr& lhs, thread_specific_ptr& rhs) noexcept;
-
     // disable copy from lvalue
     thread_specific_ptr(const thread_specific_ptr&) = delete;
     thread_specific_ptr& operator=(const thread_specific_ptr&) = delete;
+    // disable move
+    thread_specific_ptr(thread_specific_ptr&&) = delete;
+    thread_specific_ptr& operator=(thread_specific_ptr&&) = delete;
 
 private:
-    unique_ptr<T, Deleter> p;       // exposition only
+    Deleter del;                    // exposition only
 };
 
 } // namespace std
